a65c9f772414f3f7de9f9c2102e704de2111cce3
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-29 15:41:26 +0100

    gestione click del primo menu fatta

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+//mi creo le variabili contententi le parti principali
 var buttonMenu = $('#buttonMenu');
-console.log(buttonMenu);
+var nav = $('nav');
+var elemento = $('nav .elemento');
 
+console.log(buttonMenu);
+console.log(elemento);
+
+//gestisco il click per il primo menù
 buttonMenu.click(function(){
-  alert('ciao');
+  //alert('ciao');
+  nav.toggle();
+});
+
+//gestisco l'hover
+elemento.mouseover(function(){
+  console.log("over");
 });
